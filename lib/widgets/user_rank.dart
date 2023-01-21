57fc21a257c44fdc89a75878d6b7cbd8ef50b5e8
nowakmikolaj <nowak.mikolaj19@gmail.com>
2023-01-21 19:19:54 +0100

    Select current user on the ranking list

--- lib/widgets/user_rank.dart
+++ lib/widgets/user_rank.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:football_app/models/user_rank_data.dart';
 import 'package:football_app/utils/app_padding.dart';
@@ -22,47 +23,57 @@ class UserRank extends StatelessWidget {
       (userRankData.average.toStringAsFixed(2)),
     ];
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: AppPadding.p2,
-        horizontal: AppPadding.p15,
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(AppSize.s15),
+        color: userRankData.userId == FirebaseAuth.instance.currentUser!.email
+            ? Theme.of(context).brightness == Brightness.dark
+                ? Colors.grey[700]
+                : Colors.grey[400]
+            : Colors.transparent,
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(children: [
-            SizedBox(
-              width: AppSize.s30,
-              height: AppSize.s20,
-              child: Text(
-                '${rank.toString()}.',
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: FontSize.bodyText),
-              ),
-            ),
-            Text(
-              userRankData.userId.split('@').first,
-              style: const TextStyle(fontSize: FontSize.bodyText),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ]),
-          Row(
-            children: [
-              ...List.generate(
-                teamData.length,
-                (index) => SizedBox(
-                  width: AppSize.s46,
-                  child: Text(
-                    teamData[index],
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(fontSize: FontSize.bodyText),
-                  ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: AppPadding.p2,
+          horizontal: AppPadding.p15,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(children: [
+              SizedBox(
+                width: AppSize.s30,
+                height: AppSize.s20,
+                child: Text(
+                  '${rank.toString()}.',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: FontSize.bodyText),
                 ),
               ),
-            ],
-          )
-        ],
+              Text(
+                userRankData.userId.split('@').first,
+                style: const TextStyle(fontSize: FontSize.bodyText),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ]),
+            Row(
+              children: [
+                ...List.generate(
+                  teamData.length,
+                  (index) => SizedBox(
+                    width: AppSize.s46,
+                    child: Text(
+                      teamData[index],
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: FontSize.bodyText),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
